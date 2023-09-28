7cd828abfbfffc4f8e10a800b0523a41cb36cbd8
jeongwwon <spwldmlgla@naver.com>
2023-09-28 21:39:06 +0900

    Token

--- djangoenv/blog/views.py
+++ djangoenv/blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from .models import Post
+
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+class IntruderImage(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
