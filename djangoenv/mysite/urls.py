f7fd9263f455faa97d57a5f9644ee2b804fb399a
jeongwwon <spwldmlgla@naver.com>
2023-09-29 00:44:06 +0900

    Error

--- djangoenv/mysite/urls.py
+++ djangoenv/mysite/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #a)
     path('', include('blog.urls')),
     path('api-token-auth/', obtain_auth_token), #added
 ]
