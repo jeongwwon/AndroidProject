8cab498f38da9ea6a620656f4f18a894addac570
jeongwwon <spwldmlgla@naver.com>
2023-09-29 00:54:59 +0900

    Last

--- djangoenv/mysite/wsgi.py
+++ djangoenv/mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
