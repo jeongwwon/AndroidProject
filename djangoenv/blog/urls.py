521cc474d7ced50dc5fe0f6749fc471e7c2cabbc
jeongwwon <spwldmlgla@naver.com>
2023-10-24 19:40:26 +0900

    NEW

--- djangoenv/blog/urls.py
+++ djangoenv/blog/urls.py
@@ -7,5 +7,6 @@ router.register('Post', views.IntruderImage)
 
 urlpatterns = [
 path('', views.post_list, name='post_list'),
+ path('post/<int:pk>/', views.post_detail, name='post_detail'),
 path('api_root/', include(router.urls)),
 ]
--- djangoenv/blog/views.py
+++ djangoenv/blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import PostSerializer
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()  # Assuming you want to fetch all posts
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 class IntruderImage(viewsets.ModelViewSet):
     queryset = Post.objects.all()
