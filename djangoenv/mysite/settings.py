8788de7eecae0a68c2a27e6bfeb0bca941469855
JeongWon <spwldmlgla@naver.com>
2023-11-14 23:57:18 +0900

    CORS

--- djangoenv/mysite/settings.py
+++ djangoenv/mysite/settings.py
@@ -53,11 +53,12 @@ REST_FRAMEWORK = {
 MIME_TYPES = {
     '.css': 'text/css',
 }
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
