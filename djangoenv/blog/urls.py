c7767898d278405cc9229f53e64a9d6d8f27ec5f
JeongWon <spwldmlgla@naver.com>
2023-11-17 18:22:22 +0900

    Login

--- djangoenv/blog/urls.py
+++ djangoenv/blog/urls.py
@@ -14,6 +14,7 @@ path('post_list', views.post_list, name='post_list'),
 path('api_root/', include(router.urls)),
 path('register/', views.register_user, name='register_user'),
 path('mobile_register/',views.mobile_register_user, name='mobile_register_user'),
+path('mobile_login/',views.mobile_login, name='mobile_login'),
 path('login/', LoginView.as_view(), name='login'),
  path('logout/', LogoutView.as_view(), name='logout'),
 ]
