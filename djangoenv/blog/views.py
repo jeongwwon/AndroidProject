2853415bca4b602a799292bde3ee797fbf2571fa
JeongWon <spwldmlgla@naver.com>
2023-11-22 16:30:43 +0900

    get

--- djangoenv/blog/views.py
+++ djangoenv/blog/views.py
@@ -52,7 +52,7 @@ def register_user(request):
 
 def get_global_count(request):
     # GlobalPostCount 모델의 count 반환
-    global_count = GlobalPostCount.objects.all()
+    global_count = GlobalPostCount.objects.get()
     return JsonResponse({'global_count': global_count})
 
 def get_today_count(request):
